822e07386a583aae63eb68eb3a36ac4f813918b0
ggomesneto <69973423+ggomesneto@users.noreply.github.com>
2020-09-29 04:53:36 -0500

    updating-portfolio

--- HackOrSnooze/api-classes.js
+++ HackOrSnooze/api-classes.js
@@ -26,6 +26,7 @@ class StoryList {
 	static async getStories() {
 		// query the /stories endpoint (no auth required)
 		const response = await axios.get(`${BASE_URL}/stories`);
+		
 
 		// turn the plain old story objects from the API into instances of the Story class
 		const stories = response.data.stories.map((story) => new Story(story));
