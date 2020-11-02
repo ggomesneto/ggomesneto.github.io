07e0ccdecdf514b335abd56ebf4fcdb9d0592275
ggomesneto <69973423+ggomesneto@users.noreply.github.com>
2020-11-01 22:25:41 -0600

    adding-about me

--- VM/app.js
+++ VM/app.js
@@ -20,7 +20,7 @@ $(document).on('dblclick', '#mySkills', function() {
 
                     </div>
 
-                </div>
+     </div>
 
     `;
 	$desktop.append(markup);
@@ -48,12 +48,12 @@ $(document).on('dblclick', '#portFile', function() {
     <div id='folderContent'>
 
         <div class='folderItem text-center firstLine' id='projects'>
-            <i class="fas fa-folder fa-5x min"></i><br>
+        <i class="far fa-folder fa-5x"></i></i><br>
             <span>Projects</span>
         </div>
 
         <div class='folderItem text-center firstLine' id='aboutMe'>
-            <i class="fas fa-folder fa-5x min"></i><br>
+        <i class="far fa-file-powerpoint fa-5x"></i><br>
             <span>About Me</span>
         </div>
 
@@ -68,7 +68,7 @@ $(document).on('dblclick', '#portFile', function() {
         </div>
 
         <div class='folderItem text-center secLine' id='email'>
-            <i class="far fa-envelope-open fa-3x"></i><Br>
+            <i class="far fa-envelope-open fa-5x"></i><Br>
             <span>Email</span>
         </div>
 
@@ -104,7 +104,7 @@ $(document).on('dblclick', '#projects', function() {
     <span>Hack or Snooze</span>
 </div>
 
-<div class='folderItem text-center firstLine' id='curriculum'>
+<div class='folderItem text-center secLine' id='curriculum'>
 <img src='connect-card.png'><br>
     <span>Connect Four</span>
 </div>
@@ -154,3 +154,115 @@ $(document).on('click', '#left', function() {
 	$('#folderContent').append(markup);
 	$('#header').text('PORTFOLIO V35.A');
 });
+
+$(document).on('dblclick', '#aboutMe', function() {
+	let markup = `
+    <div id='aboutMeFile'>
+
+                    <div id='topBar'>
+
+                        <i class="far fa-image "></i><span id='vmName'>About Me</span><span id='close'>x</span>
+
+                        
+
+                    </div>
+                    
+                    <div class='text-center' id='latDetail'>
+                    
+                    <br>
+       
+        <br>
+        <br>
+        <br>
+        <br>
+        <span  class='title'>THAT'S ME WORKING OFFSHORE WHEN I WAS 23.</SPAN><br><br>
+        <span>I WAS RESPONSIBLE FOR ALL THE LOGGING-WHILE-DRILLING (LWD) DATA AND EQUIPMENT ON OFFSHORE WELLS.</SPAN><br><br>
+        <span>DURING MY TIME WORKING IN THE OILFIELD I WENT FROM AN <Strong>INTERN</Strong> IN BRAZIL TO A <Strong>SENIOR</Strong> LWD SPECIALIST, WORKING IN THE <Strong>US, RUSSIA, UAE AND A COUPLE OF OTHER COUNTRIES.</Strong> </SPAN><br><br>
+        <SPAN> THIS PHOTO WAS AFTER 43 DAYS STRAIGHT WORKING 12H/DAY. I WAS FEELING MORE PIRATE THAN EVER</SPAN>
+        <br>
+        <br>
+        <i id='right' class="fas fa-arrow-right fa-3x"></i>
+
+                    </div>
+
+     </div>`;
+	$desktop.append(markup);
+
+	let backgroundDiv = $('#aboutMeFile');
+	backgroundDiv.css('background-image', 'url(about-1.jpg)');
+
+	$(document).on('click', '#right', function() {
+		let url = backgroundDiv.css('background-image');
+
+		if (url.includes('about-1')) {
+			backgroundDiv.css('background-image', 'url(about-2.jpg)');
+
+			$('#latDetail').empty();
+
+			let markup = `
+       
+        <br>
+        <br>
+        <br>
+        <br>
+        <br>
+        <span  class='title'>THAT'S ME PLAYING POKER</SPAN><br><br>
+        <span>AFTER WORKING FOR ALMOST A DECADE OFFSHORE, I DECIDED TO TURN A HOBBY INTO A CAREER</SPAN><br><br>
+        <span>DURING <STRONG>3 YEARS</STRONG>, I TRAVELLED THROUGH BRAZIL AND THE USA PLAYING POKER. LIFE WAS GOOD, BUT THERE WAS NO FINANCIAL STABILITY. </SPAN><br><span>BUT IF I HAVE NEVER TRIED THIS, I WOULDN'T MEET THE <STRONG>LOVE OF MY LIFE</STRONG></SPAN><br>
+        <br>
+        <br>
+        <br>
+        <br>
+        <i id='right' class="fas fa-arrow-right fa-3x"></i>
+        
+        
+        `;
+			$('#latDetail').append(markup);
+		} else if (url.includes('about-2')) {
+			backgroundDiv.css('background-image', 'url(about-3.jpg)');
+
+			$('#latDetail').empty();
+
+			let markup = `
+        <br>
+        
+        
+        <span class='title' >THAT'S MY FAMILY</SPAN><br><br>
+        <span>NOW YOU PROBABLY UNDERSTAND WHY I'M TRYNG TO TRANSIT FROM THE OILFIELD TO THE TECH FIELD<br></SPAN>
+        <span>(AND IS NOT BECAUSE I PROBABLY FIT ON THE STEREOTYPE OF THE <br>'TECH GUY').</SPAN><br><br>   
+        <span>I WORKED WITH THE BEST PEOPLE AND TECHNOLOGY POSSIBLE IN THE OILFIELD, AND I WAS EXPECTED TO EXCEL IN EVERYTHING, NOT ONLY BY MY MANAGERS BUT ALSO BY MYSELF, SO I'M SURE I CAN KEEP UP WITH THE GOOD WORK AS A SOFTWARE ENGINEER. </SPAN><br><BR>
+        <span>AND I CAN BE DOING SOMETHING USEFUL AND PLEASENT AT THE SAME TIME: WORKING ON TOP NOTCH TECHNOLOGY AND NOT BEING AWAY FROM HOME ANYMORE.</SPAN>
+        <br>
+        <br>
+        <i id='right' class="fas fa-arrow-right fa-3x"></i>
+        
+        
+        `;
+			$('#latDetail').append(markup);
+		} else if (url.includes('about-3')) {
+			backgroundDiv.css('background-image', 'url(about-1.jpg)');
+			$('#latDetail').empty();
+
+			let markup = `
+    
+        <br>
+        <br>
+        <br>
+        <br>
+        <br>
+        <span  class='title'> THAT'S ME WORKING OFFSHORE WHEN I WAS 23.</SPAN><br><br>
+        <span>I WAS RESPONSIBLE FOR ALL THE LOGGING-WHILE-DRILLING (LWD) DATA AND EQUIPMENT ON OFFSHORE WELLS.</SPAN><br><br>
+        <span>DURING MY TIME WORKING IN THE OILFIELD I WENT FROM AN <Strong>INTERN</Strong> IN BRAZIL TO A <Strong>SENIOR</Strong> LWD SPECIALIST, WORKING IN THE <Strong>US, RUSSIA, UAE AND A COUPLE OF OTHER COUNTRIES.</Strong> </SPAN><br><br>
+        <SPAN> THIS PHOTO WAS AFTER 43 DAYS STRAIGHT WORKING 12H/DAY. I WAS FEELING MORE PIRATE THAN EVER</SPAN>
+        <br>
+        <br>
+        <i id='right' class="fas fa-arrow-right fa-3x"></i>
+        
+        
+        `;
+			$('#latDetail').append(markup);
+		}
+	});
+});
+
+let count = 1;
