a7991c133e61793be332cd34d41b10ed3ac4b2f6
ggomesneto <69973423+ggomesneto@users.noreply.github.com>
2020-11-04 22:26:13 -0600

    changing all

--- 1-Page-Portfolio/app.js
+++ 1-Page-Portfolio/app.js
@@ -1,8 +1,43 @@
-let $close = $('#close');
-let $desktop = $('#desktop');
-
-$(document).on('click', '#close', function() {
-	let $file = $(this).parent().parent();
-
-	$file.remove();
+$(function() {
+	$('[data-toggle="tooltip"]').tooltip();
 });
+
+function myFunction(x) {
+	if (x.matches) {
+		// If media query matches
+		$('.carousel-item').remove();
+		$carousel.append(markupBoth);
+	} else {
+		$('.carousel-item').remove();
+		$carousel.append(markupFull);
+	}
+}
+
+let $carousel = $('.carousel-inner');
+
+let markupFull = `
+<div class="carousel-item active">
+                      
+<img class="p-3" src="jeopardy.jpg" alt="First slide">
+<img class="p-3" src="connect-card.png" alt="Second slide">
+<img class="p-3" src="meme-card.png" alt="Third slide">
+<img class="p-3" src="Hacker-News.jpg" alt="Third slide">
+</div>
+`;
+
+let markupBoth = `
+<div class="carousel-item active">
+                      
+<img class="p-3" src="jeopardy.jpg" alt="First slide">
+<img class="p-3" src="connect-card.png" alt="Second slide">
+</div>
+<div class="carousel-item ">
+<img class="p-3" src="meme-card.png" alt="Third slide">
+<img class="p-3" src="Hacker-News.jpg" alt="Third slide">
+</div>
+
+`;
+
+var x = window.matchMedia('(max-width: 700px)');
+myFunction(x); // Call listener function at run time
+x.addListener(myFunction); // Attach listener function on state changes
