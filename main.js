4abf36fd05a641d6d8ca92d9089705023b9f22af
ggomesneto <69973423+ggomesneto@users.noreply.github.com>
2020-12-23 18:51:33 -0600

    fixes

--- main.js
+++ main.js
@@ -1,119 +1,13 @@
-
-
-$(document).on('click', '#about', function() {
-	if ($('#modal').hasClass('show')) {
-	} else {
-		$('#modal').toggleClass('show hide');
-	}
-	if ($('#display').hasClass('grid')) {
-		$('#display').toggleClass('grid hide');
-	}
-	$('#modal-text').empty();
-	let markup = `
-	
-	<p>Hey, I'm Geraldo. 
-                        <p>
-                            <i class="fas fa-laptop-code"></i> Software Engineer |
-                            <i class="fas fa-cloud"></i> AWS Certified |
-							<i class="fas fa-code"></i> Boot Camp Graduate |
-							<i class="fas fa-tachometer-alt"></i> Fast Learner |
-							<i class="fas fa-globe-americas"></i> International Experience</p>
-
-                    <p class='about-me'>I AM A BRAZILIAN LIVING IN THE USA SINCE 2018. MY CAREER IN THE OILFIELD STARTED WHEN I WAS 21 AND ON THE YEARS AFTER I WORKED IN PLACES LIKE SIBERIA, ABU DHABI, BRAZIL AND CENTRAL AMERICA. EVEN THOUGH IT WAS A VERY EXCITING CAREER, IN 2016 I DECIDED TO PURSUE A DREAM AND BECAME A FULL-TIME POKER PLAYER IN LAS VEGAS. FOR THREE YEARS I SUCCEED BUT, AFTER GETTING MARRIED, I DECIDED TO GO BACK TO WORK IN A 'NORMAL' JOB THAT COULD GIVE ME MORE FINANCIAL STABILITY.</p>
-					<p class='about-me'>AFTER WORKING WITH DIFFERENT TYPES OF TECHNOLOGY, I FOUND LOVE IN CODING AND DECIDED TO TAKE A STEP OUT OF MY COMFORT ZONE AND LEARN MORE ABOUT SOFTWARE ENGINEERING. WITH THE KNOWLEDGE FROM THE SOFTWARE ENGINEERING BOOTCAMP AT SPRINGBOARD I FEEL VERY COMFORTABLE TO PURSUE A CAREER CHANGE AND TRAIL A NEW PATH, NOW AS A DEVELOPER INSTEAD OF SOMEONE THAT JUST USES THE FINAL PRODUCT.</p>
-					<p class='about-me'> I AM PURSUING A SOFTWARE ENGINEER POSITION IN WHICH I CAN CONTINUE PROGRESSING AS A DEVELOPER, BE PART OF A GREAT TEAM AND SHARE MY KNOWLEDGE WITH OTHERS.</P>
-	
-	
-					
-	
-	`;
-
-	$('#modal-text').append(markup);
-});
-
-$(document).on('click', '#portfolio', function() {
-
-	if ($('#modal').hasClass('show')){
-		$('#modal').toggleClass('show hide')
-	}
-
-	if ($('#display').hasClass('hide')){
-		$('#display').toggleClass('grid hide');
-		
-	} 
-	
-	
-});
-
-$(document).on('click', '#toolkit', function() {
-
-	if ($('#modal').hasClass('show')) {
-	} else {
-		$('#modal').toggleClass('show hide');
-	}
-	if ($('#display').hasClass('grid')) {
-		$('#display').toggleClass('grid hide');
-	}
-
-
-	$('#modal-text').empty();
-	let markup = `
-	
-	<p> 
-                        <img data-toggle="tooltip" data-placement="top" title="BootStrap" src='img/bootstrap.png'>
-                        <img data-toggle="tooltip" data-placement="top" title="CSS" src='img/css.png'>
-                        <img data-toggle="tooltip" data-placement="top" title="Git" src='img/git.png'>
-                        <img data-toggle="tooltip" data-placement="top" title="HTML5" src='img/html5.png'>
-                        <img data-toggle="tooltip" data-placement="top" title="JavaScript" src='img/javascript.png'>
-                        <img data-toggle="tooltip" data-placement="top" title="MongoDB" src='img/mongodb.png'><br>
-                        <img data-toggle="tooltip" data-placement="top" title="NodeJS" src='img/nodejs.png'>
-                        <img data-toggle="tooltip" data-placement="top" title="PostgreSQL" src='img/postgres.png'>
-                        <img data-toggle="tooltip" data-placement="top" title="Python" src='img/python.png'>
-                        <img data-toggle="tooltip" data-placement="top" title="React" src='img/react.png'>
-						<img data-toggle="tooltip" data-placement="top" title="VSCode" src='img/vscode1.png'></img>
-						
-	</p>
-	<p> I FOCUS ON THE MERN STACK BUT I'M <strong>NOT RESTRICTED</strong> TO IT.<br> I VALUE A 'ALWAYS LEARNING' MENTALITY AND I CAN TACKLE ANY OBSTACLES AHEAD OF ME.</P>
-	`;
-
-	// $('#modal').css({
-	// 	'display': 'grid',
-	// 	'grid-template-columns' : 'repeat(auto-fill, minmax(350px, 1fr))' })
-	
-	$('#modal-text').append(markup);
-	$(function() {
-		$('[data-toggle="tooltip"]').tooltip();
-	});
-});
-
-$(document).on('click', '#contact', function() {
-	if ($('#modal').hasClass('show')) {
-	} else {
-		$('#modal').toggleClass('show hide');
-	}
-	if ($('#display').hasClass('grid')) {
-		$('#display').toggleClass('grid hide');
-	}
-
-	$('#modal-text').empty();
-	let markup = `
-	
-	<p class='contact' ><a href='https://linkedin.com/in/ggomesneto' target='_blank'>LINKEDIN</a> | <a href='https://github.com/ggomesneto' target='_blank'>GITHUB</a> | <A href='https://drive.google.com/file/d/18Ba4_hL8BmGvszqbBasiC02MONelZA2w/view?usp=sharing' target='_blank'>CURRICULUM</A> | GGOMESNETO@GMAIL.COM</P>
-	
-	
-	
-	`;
-
-	$('#modal-text').append(markup);
-});
-
-$(document).on('click', '#modal-back', function() {
-	$('#modal-back').remove();
-	$('#modal').remove();
-});
-
 $(document).on('click', '#fancy', function() {
 	fancyIntro();
+});
+
+$(document).on('click', '#me', function() {
+	$('#footer').toggleClass('show hide');
+	$(this).css('width', '150px');
+	if ($('#footer').hasClass('hide')) {
+		$(this).css('width', '50px');
+	}
 });
 
 function fancyIntro() {
@@ -145,7 +39,6 @@ function loopMe() {
 setInterval(function() {
 	loopMe();
 }, 5000);
-
 
 setTimeout(function() {
 	$('#modal').toggleClass('show hide');
